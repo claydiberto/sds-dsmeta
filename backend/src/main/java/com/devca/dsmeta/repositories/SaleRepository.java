99eaba194295647c134827301619acb1c6b24c14
claydiberto <claydiberto@gmail.com>
2022-09-08 15:38:52 -0300

    API test

---
+++ backend/src/main/java/com/devca/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devca.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devca.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
