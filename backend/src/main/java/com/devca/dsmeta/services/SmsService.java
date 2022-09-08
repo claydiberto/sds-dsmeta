73128d2d37ed781030a1918b67d540e07d22fcc8
claydiberto <claydiberto@gmail.com>
2022-09-08 17:15:55 -0300

    Heroku

--- backend/src/main/java/com/devca/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devca/dsmeta/services/SmsService.java
@@ -34,7 +34,7 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "Parabéns " + sale.getSellerName() + ". Você foi destaque no mês " + date
+		String msg = "Parabéns " + sale.getSellerName() + "! Você foi destaque no mês " + date
 				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
